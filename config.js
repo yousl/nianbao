b4545f131aa461d31afd1aca012d931aa193bd33
yousl <youster@163.com>
2017-12-22 12:54:33 +0800

    a

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   cookieName: 'your_cookie_name',
-  mongodbUrl: 'mongodb://localhost:27017/firstapp',
+  mongodbUrl: 'mongodb://35.194.198.25:27017/first-app',
   admin: 'admin',
   sessionSecret: 'your_session_secret'
 }
