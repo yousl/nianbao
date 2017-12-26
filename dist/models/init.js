62a085218376967cc5938ff74a4d0f8775de6564
yousl <youster@163.com>
2017-12-26 19:41:56 +0800

    a

--- dist/models/init.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var mongoose = require('mongoose');
-var config = require('../config');
-
-mongoose.connect(config.mongodbUrl, {
-  useMongoClient: true
-});
-//# sourceMappingURL=init.js.map
